5781481f152612012d259459bb6208e047a27393
Sam Jones <sam@haganjones.com>
2017-01-23 13:48:35 +0000

    Initial Work

---
+++ tests/Unit/InitialisationTest.php
@@ -0,0 +1,64 @@
+<?php
+namespace Tests\Unit;
+
+use Sumy\Exceptions\InitialisationException;
+use Sumy\Exceptions\NaNException;
+use Sumy\Sumy;
+use Tests\TestCase;
+
+class InitialisationTest extends TestCase
+{
+    /** @test */
+    public function is_initialised_with_current_number_at_zero()
+    {
+        $sumy = new Sumy();
+
+        $this->assertTrue($sumy instanceof Sumy);
+        $this->assertEquals(0, $sumy->get());
+    }
+
+    /** @test */
+    public function is_initialised_with_passed_number()
+    {
+        $sumy = new Sumy(300);
+
+        $this->assertEquals(300, $sumy->get());
+    }
+
+    /** @test */
+    public function is_initialised_with_instance_of_sumy()
+    {
+        $sumy = new Sumy(new Sumy(300));
+
+        $this->assertEquals(300, $sumy->get());
+    }
+
+    /** @test */
+    public function can_be_initialised_with_null()
+    {
+        $sumy = new Sumy(null);
+
+        $this->assertEquals(0, $sumy->get());
+    }
+
+    /** @test */
+    public function cannot_be_initialised_with_a_string()
+    {
+        $this->expectException(NaNException::class);
+        new Sumy('hello');
+    }
+
+    /** @test */
+    public function cannot_be_initialised_with_a_object()
+    {
+        $this->expectException(NaNException::class);
+        new Sumy((new \stdClass()));
+    }
+
+    /** @test */
+    public function can_be_initialised_with_a_null()
+    {
+        $this->expectException(NaNException::class);
+        new Sumy(array());
+    }
+}
---
+++ tests/Unit/SubtractionTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Tests\Unit;
+
+use Sumy\Sumy;
+use Tests\TestCase;
+
+class SubtractionTest extends TestCase
+{
+    /** @test */
+    public function can_subtract_a_number()
+    {
+        $sumy = new Sumy(456.9105);
+
+        $sumy->subtract(346.9105);
+        $this->assertEquals(110, $sumy->get());
+    }
+
+    /** @test */
+    public function can_chain_subtract_a_number()
+    {
+        $sumy = new Sumy(456.9105);
+
+        $sumy->subtract(346.9105)->subtract(3.7);
+        $this->assertEquals(106.3, $sumy->get());
+    }
+}
